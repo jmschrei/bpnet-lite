a557d959d24d3198cf49c35711140beecdafb829
jacob.schreiber <jacob.schreiber@clip-c2-59.cbe.vbc.ac.at>
2025-07-04 14:41:19 +0200

    v0.9.1

--- bpnetlite/__init__.py
+++ bpnetlite/__init__.py
@@ -1,8 +1,8 @@
 # bpnet-lite
 # Author: Jacob Schreiber
-# Code adapted from Avanti Shrikumar and Ziga Avsec
+# Code adapted from Avanti Shrikumar and Ziga Avsec and Alex Tseng and many others
 
 from .bpnet import BPNet
 from .chrombpnet import ChromBPNet
 
-__version__ = '0.9.0'
+__version__ = '0.9.1'
